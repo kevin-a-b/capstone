c4ecf1c3f16849fcf754be5ef37326cec233d537
Kevin Brown <kevin@ancillapartners.com>
2021-03-31 19:30:47 -0500

    aws sdk plus initial code

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'config.php';
+
+$host = "host";
+$username = "capstone";
+$password = "password";
+$dbname = "";
+$port = 80;
+
+$connection = new mysqli($host, $username, $password, $dbname, $port);
+
