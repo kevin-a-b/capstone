089fb08b2ddf9e649228a540fa48812f402b22be
Kevin Brown <kbrownfc1105@yahoo.com>
2021-04-20 00:20:06 -0500

    mysqli test, rm vendor from git

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-var_dump($_SERVER);
+//var_dump($_SERVER);
 //$dbhost = $_SERVER['aa1g1tnfvy31umg.cqupwsiydadl.us-east-2.rds.amazonaws.com'];
 //$dbport = $_SERVER['3306'];
 //$dbname = $_SERVER['ebdb'];
@@ -10,6 +10,26 @@ var_dump($_SERVER);
 //$password = $_SERVER['password'];
 //
 //$pdo = new PDO($dsn, $username, $password);
+$host = 'aa1g1tnfvy31umg.cqupwsiydadl.us-east-2.rds.amazonaws.com';
+$dbname = 'ebdb';
+$port = 3306;
+$username = 'capstone';
+$password = 'password';
+
+$link = new mysqli($host, $username, $password, $dbname, $port);
+
+if ($link -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $link -> connect_error;
+    exit();
+}
+
+if ($result = $link -> query("select * from user_account")) {
+    echo "Returned rows are: " . $result -> num_rows;
+    // Free result set
+    $result -> free_result();
+}
+
+$link -> close();
 
 ?>
 
