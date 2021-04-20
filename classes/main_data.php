f0201cd634db3d2e52e28d454df07571ca76319e
Kevin Brown <kbrownfc1105@yahoo.com>
2021-04-20 01:12:19 -0500

    update correct table name

--- classes/main_data.php
+++ classes/main_data.php
@@ -5,7 +5,7 @@ class MainData extends DB_Sql
 {
 
     function GetUsers(){
-        $queryString = "select * from users";
+        $queryString = "select * from user_account";
         $this->query($queryString);
     }
 
