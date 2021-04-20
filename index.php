2c556b2c20c13c41bd17c0f5d9d61d9881381620
Kevin Brown <kbrownfc1105@yahoo.com>
2021-04-19 23:43:14 -0500

    var dump server

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+var_dump($_SERVER);
+//$dbhost = $_SERVER['aa1g1tnfvy31umg.cqupwsiydadl.us-east-2.rds.amazonaws.com'];
+//$dbport = $_SERVER['3306'];
+//$dbname = $_SERVER['ebdb'];
+//$charset = 'utf8' ;
+//
+//$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+//$username = $_SERVER['capstone'];
+//$password = $_SERVER['password'];
+//
+//$pdo = new PDO($dsn, $username, $password);
 
 ?>
 
