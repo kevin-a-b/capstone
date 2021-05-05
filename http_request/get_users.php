f63666da558e6d6e7ef28a81e1b2909f67caef9d
Kevin Brown <kbrownfc1105@yahoo.com>
2021-05-05 16:32:04 -0500

    uncomment code for testing

--- http_request/get_users.php
+++ http_request/get_users.php
@@ -5,22 +5,22 @@ ini_set("display_errors","On");
 include("../classes/config.php");
 $Config = new Config();
 
-//$Users = new MainData();
-//$Users->GetUsers();
+$Users = new MainData();
+$Users->GetUsers();
 
-//$json_users = [];
+$json_users = [];
 
-//while($Users->next_record()){
-//    array_push($json_users, array('id' => $Users->f('Account_ID'), 'user_name' => $Users->f('Account_Username')));
-//}
+while($Users->next_record()){
+    array_push($json_users, array('id' => $Users->f('Account_ID'), 'user_name' => $Users->f('Account_Username')));
+}
 
 http_response_code(202);    // OK
 
-//echo json_encode(
-//    array(
-//        "users" => $json_users
-//    )
-//);
+echo json_encode(
+    array(
+        "users" => $json_users
+    )
+);
 
 //$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
 //
