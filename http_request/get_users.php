c2b389b8435aeb5da7db7cf63b96989266519217
Kevin Brown <kbrownfc1105@yahoo.com>
2021-05-05 11:11:29 -0500

    testing http response code

--- http_request/get_users.php
+++ http_request/get_users.php
@@ -3,22 +3,22 @@
 include("classes/config.php");
 $Config = new Config();
 
-$Users = new MainData();
-$Users->GetUsers();
-
-$json_users = [];
-
-while($Users->next_record()){
-    array_push($json_users, array('id' => $Users->f('account_id'), 'user_name' => $Users->f('user_name')));
-}
+//$Users = new MainData();
+//$Users->GetUsers();
+//
+//$json_users = [];
+//
+//while($Users->next_record()){
+//    array_push($json_users, array('id' => $Users->f('account_id'), 'user_name' => $Users->f('user_name')));
+//}
 
 http_response_code(200);    // OK
 
-echo json_encode(
-    array(
-        "users" => $json_users
-    )
-);
+//echo json_encode(
+//    array(
+//        "users" => $json_users
+//    )
+//);
 
 
 //account_id
